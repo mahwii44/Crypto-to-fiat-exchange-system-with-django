e90656bda1a4595c2dc41318baa9ca725129b094
Opeyemi Michael <70078674+mahwii44@users.noreply.github.com>
2023-03-28 01:23:55 +0000

    modified:   cryptoex/swapper/models.py 	modified:   cryptoex/swapper/views.py

--- cryptoex/swapper/models.py
+++ cryptoex/swapper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ConversionRate(models.Model):
+    from_currency = models.CharField(max_length=10)
+    to_currency = models.CharField(max_length=10)
+    rate = models.FloatField()
+
+    def __str__(self):
+        return f'{self.from_currency} to {self.to_currency}: {self.rate}'
--- cryptoex/swapper/views.py
+++ cryptoex/swapper/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import ConversionRate
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    conversion_rate = ConversionRate.objects.get(from_currency='BTC', to_currency='USD')
+    btc_amount = float(request.GET.get('btc_amount'))
+    usd_amount = btc_amount * conversion_rate.rate
+    
+    return render(request, 'index.html', {'amount':usd_amount})
