c7861bb9426667e0934647387d3b09475e78204a
Opeyemi Michael <70078674+mahwii44@users.noreply.github.com>
2023-03-29 23:10:51 +0000

    modified:   cryptoex/swapper/models.py

--- cryptoex/swapper/models.py
+++ cryptoex/swapper/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class ConversionRate(models.Model):
+    from_currency = models.CharField(max_length=10)
+    to_currency = models.CharField(max_length=10)
+    rate = models.FloatField()
+
+    def __str__(self):
+        return f'{self.from_currency} to {self.to_currency}: {self.rate}'
+    
 class CryptoCurrency(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10)
