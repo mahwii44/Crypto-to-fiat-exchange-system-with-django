6b256a46eea21e6f954563e5bba3f28cfe7483fd
Opeyemi Michael <70078674+mahwii44@users.noreply.github.com>
2023-03-30 00:30:14 +0000

    modified:   Added API for getting live cryptodata

--- cryptoex/swapper/views.py
+++ cryptoex/swapper/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from urllib import response
 ##from .models import ConversionRate
 from .models import CryptoCurrency
+import requests
 # Create your views here.
 
 #def index(request):
@@ -34,3 +36,34 @@ def index(request):
         }
 
     return render(request, 'index.html', context)
+
+# Function to get cryptocurrency data from the CoinMarketCap API
+def get_crypto_data():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'your_api_key_here'
+}
+    parameters = {
+    'symbol': 'BTC',
+    'convert': 'USD'
+}
+
+    response = requests.get(url, headers=headers, params=parameters)
+
+if response.status_code == 200:
+    data = response.json()
+    price = data['data']['BTC']['quote']['USD']['price']
+    print(f"Latest BTC-USD price: {price:.2f}")
+else:
+    print(f"Error {response.status_code}: {response.reason}")
+
+# Function to convert the selected cryptocurrency amount to USD
+def convert_to_usd(crypto, amount):
+    data = get_crypto_data()
+    for item in data:
+        if item['symbol'] == crypto:
+            price = float(item['price_usd'])
+            converted_amount = round(price * float(amount), 2)
+            return converted_amount
+    return None
